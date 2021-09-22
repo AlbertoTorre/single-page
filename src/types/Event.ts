5edb300f09604ce83aa9fd3cbd8a84879e36aa48
Jefe_TI <dieoch>
2021-09-22 12:16:43 -0500

    definición de tipos

---
+++ src/types/Event.ts
@@ -0,0 +1,8 @@
+export default interface Event {
+    id: null;
+    image: string;
+    title: string;
+    description: string;
+    tags: object;
+    online: boolean;
+}
---
+++ src/types/Project.ts
@@ -0,0 +1,8 @@
+export default interface Project {
+    id: null;
+    image: string;
+    title: string;
+    description: string;
+    tags: object;
+    featured: boolean;
+}
