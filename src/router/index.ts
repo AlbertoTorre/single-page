887bd71001b9a444befb755707b15122ecd63b41
Jefe_TI <dieoch>
2021-09-22 12:17:53 -0500

    registro de rutas

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,12 +8,14 @@ const routes: Array<RouteRecordRaw> = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/events',
+    name: 'Events',
+    component: () => import('../views/Events.vue')
+  },
+  {
+    path: '/event/:id',
+    name: 'Event',
+    component: () => import('../views/Event.vue')
   }
 ]
 
